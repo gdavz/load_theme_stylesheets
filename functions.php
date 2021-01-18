6392febb3af0f45ce4dc27b0eb2f9918f017e3dc
gdavz <gdavz22@gmail.com>
2021-01-18 16:14:13 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -30,3 +30,50 @@ function remove_core_updates(){
 global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
 add_filter('pre_site_transient_update_themes','remove_core_updates');
+
+// adds widget areas
+function register_widget_areas() {
+
+  register_sidebar( array(
+    'name'          => 'Footer area one',
+    'id'            => 'footer_area_one',
+    'description'   => 'This widget area discription',
+    'before_widget' => '<section class="footer-area footer-area-one">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+    'name'          => 'Footer area two',
+    'id'            => 'footer_area_two',
+    'description'   => 'This widget area discription',
+    'before_widget' => '<section class="footer-area footer-area-two">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+    'name'          => 'Footer area three',
+    'id'            => 'footer_area_three',
+    'description'   => 'This widget area discription',
+    'before_widget' => '<section class="footer-area footer-area-three">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+    'name'          => 'Footer area four',
+    'id'            => 'footer_area_four',
+    'description'   => 'This widget area discription',
+    'before_widget' => '<section class="footer-area footer-area-three">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
+  ));
+
+}
+
+add_action( 'widgets_init', 'register_widget_areas' );
